2484c493ccc4f01a2939246b2eb80df102e5d87b
abdelrahman455-5 <abdelrahman13205@gmail.com>
2025-11-17 07:16:03 +0200

    Adding Lesson

---
+++ Lesson.java
@@ -0,0 +1,27 @@
+package BackEnd;
+
+import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lesson {
+    String lessonId;
+    String title;
+    String content;
+    List<String> resources = new ArrayList<>();
+
+    public Lesson() {}
+
+    public Lesson(String title, String content) {
+        this.lessonId = UUID.randomUUID().toString();
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getLessonId() { return lessonId; }
+    public String getTitle() { return title; }
+    public String getContent() { return content; }
+    public List<String> getResources() { return resources; }
+
+    public void addResource(String r) { if (r != null) resources.add(r); }
+}
